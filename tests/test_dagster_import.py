224c400765f466cbd5e6b7a79ea8346faeb26d6f
Birane <elbiraneba05@gmail.com>
2026-09-19 12:37:42 +0000

    Fix test_dagster_import : resolve_asset_graph (compat Dagster récent)

--- tests/test_dagster_import.py
+++ tests/test_dagster_import.py
@@ -3,5 +3,6 @@ import dagster_orchestrator.definitions as d
 
 def test_dagster_defs_loads():
     assert d.defs is not None
-    asset_keys = {str(k) for k in d.defs.get_asset_graph().get_all_asset_keys()}
+    asset_graph = d.defs.resolve_asset_graph()
+    asset_keys = {str(k) for k in asset_graph.get_all_asset_keys()}
     assert asset_keys
